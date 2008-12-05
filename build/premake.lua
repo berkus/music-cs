d91bc128b641106bcee7c6bfba264f9626ab91a7
mjsmagalhaes <mjsmagalhaes@a630a9a0-c198-11dd-84b3-df5d23adf0f1>
2008-12-05 15:53:36 +0000

    FIX: error when premake --clean but disable it ... still need to find an workaround.

--- build/premake.lua
+++ build/premake.lua
@@ -14,50 +14,58 @@ function MakeManagedProjects()
 	dofile("MusiC.Extensions.Windows.Hamming.lua")
 end
 
-addoption("unmanaged", "Switch to build CPP code when it is available.")
+function CreateProject()
+	addoption("unmanaged", "Switch to build CPP code when it is available.")
 
-base_deps_dir="../../deps"
-base_src_dir="../../src"
-base_bin_dir="../bin"
+	base_deps_dir="../../deps"
+	base_src_dir="../../src"
+	base_bin_dir="../bin"
 
-project.name="MusiC"
-project.bindir=base_bin_dir
-project.configs={"Debug", "Release"}
-project.path="./"..target
-
-if
-target=="vs2003" or
-target=="vs2005" or
-target=="vs2008"
-then
-	--Visual Studio supports both managed and unmanaged, but separately
-	if options["unmanaged"] then
-        project.path=project.path.."-unmanaged"
-		MakeUnmanagedProjects()
-	else
-        project.path=project.path.."-managed"
+	project.name="MusiC"
+	project.bindir=base_bin_dir
+	project.configs={"Debug", "Release"}
+	project.path="./"..target
+	
+	if
+	target=="vs2003" or
+	target=="vs2005" or
+	target=="vs2008"
+	then
+		--Visual Studio supports both managed and unmanaged, but separately
+		if options["unmanaged"] then
+			project.path=project.path.."-unmanaged"
+			MakeUnmanagedProjects()
+		else
+			project.path=project.path.."-managed"
+			MakeManagedProjects()
+		end
+	elseif
+	target=="sharpdev" or
+	target=="monodev"
+	then
+		-- Those support only managed
 		MakeManagedProjects()
+	elseif
+	target=="cb-gcc" or
+	target=="cb-ow" or
+	target=="cl-gcc" or
+	target=="vs2002" or
+	target=="vs6"
+	then
+		-- Those support only unmanaged code
+		-- vs2002 dont support .net 2.0
+		MakeUnmanagedProjects()
+	elseif
+	target=="gnu"
+	then
+		-- Build both
+		MakeManagedProjects()
+		MakeUnmanagedProjects()
 	end
-elseif
-target=="sharpdev" or
-target=="monodev"
-then
-    -- Those support only managed
-    MakeManagedProjects()
-elseif
-target=="cb-gcc" or
-target=="cb-ow" or
-target=="cl-gcc" or
-target=="vs2002" or
-target=="vs6"
-then
-	-- Those support only unmanaged code
-	-- vs2002 dont support .net 2.0
-	MakeUnmanagedProjects()
-elseif
-target=="gnu"
-then
-	-- Build both
-	MakeManagedProjects()
-	MakeUnmanagedProjects()
 end
+
+function main()
+	if target~=nil then CreateProject() end
+end
+
+main()
