ac0392de618b5d80d601b6118cdcdbb6180fb602
mjsmagalhaes <mjsmagalhaes@a630a9a0-c198-11dd-84b3-df5d23adf0f1>
2008-12-15 19:00:39 +0000

    UPDATE: Algorithm.Add() error handling and documentation.

--- src/MusiC/Algorithm.cs
+++ src/MusiC/Algorithm.cs
@@ -6,29 +6,65 @@ using MusiC.Extensions;
 
 namespace MusiC
 {
-	public class Algorithm
+	public class Algorithm : MusiCObject
 	{
 		Window _window = null;
 		Classifier _classifier = null;
 		LinkedList<Feature> _featureList = new LinkedList<Feature>();
 		
-		public void Add(String extensionClass, ParamList args)
+		/// <summary>
+		/// Add a extension to the algorithm.
+		/// </summary>
+		/// <description>
+		/// It adds a feature, window or classifier to the algorithm. It supports only one window and one classifier(which is not required).
+		/// Several features may be added.
+		/// </description>
+		/// <exception cref="MusiC.Extensions.MissingExtensionException">When the exception cache cant find the class we are adding.</exception>
+		/// <param name="extensionClass">The name of the class that should be added</param>
+		/// <param name="args">The arguments to the constructor call. It must be in the correct order.</param>
+		/// <returns>Success</returns>
+		public bool Add(String extensionClass, ParamList args)
 		{
 			ExtensionInfo info = Global<ExtensionCache>.GetInstance().GetInfo(extensionClass);
+			
+			if(info == null)
+				throw new Exceptions.MissingExtensionException(extensionClass + " wasn't found.");
+			
+			Extension ext;
+			
 			switch(info.Type)
 			{
 				case ExtensionType.Classifier:
-					_classifier = info.Instantiate(args) as Classifier;
+					ext = info.Instantiate(args);
+					
+					if(typeof(Classifier).IsAssignableFrom(ext.GetType()))
+						_classifier = ext as Classifier;
+					else
+						Warning("The classifier "+extensionClass+" doesnt inherit MusiC.Classifier.");
 					break;
+					
 				case ExtensionType.Window:
-					_window = info.Instantiate(args) as Window;
+					ext = info.Instantiate(args);
+					
+					if(typeof(Window).IsAssignableFrom(ext.GetType()))
+						_window = ext as Window;
+					else
+						Warning("The window "+extensionClass+" doesnt inherit MusiC.Classifier.");
 					break;
+					
 				case ExtensionType.Feature:
-					_featureList.AddLast(info.Instantiate(args) as Feature);
+					ext = info.Instantiate(args);
+					
+					if(typeof(Feature).IsAssignableFrom(ext.GetType()))
+						_featureList.AddLast(ext as Feature);
+					else
+						Warning("The feature "+extensionClass+" doesnt inherit MusiC.Classifier.");
 					break;
 				default:
-					break;
+					return false;
 			}
+			
+			return true;
 		}
 	}
 }
