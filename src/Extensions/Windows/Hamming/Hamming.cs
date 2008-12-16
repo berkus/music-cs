f818b1091953a8fe78b55b0a8b36e3dfda90a962
mjsmagalhaes <mjsmagalhaes@a630a9a0-c198-11dd-84b3-df5d23adf0f1>
2008-12-16 17:17:56 +0000

    ADD: MusiC.Extensions.Windows.Hamming. An extensions to represent a Hamming window.

--- src/Extensions/Windows/Hamming/Hamming.cs
+++ src/Extensions/Windows/Hamming/Hamming.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 
+using MusiC;
+
 //namespace MCModule.Windows
 //{
 //	public class Hamming : MCModule.Window
@@ -17,3 +19,19 @@ using System.Text;
 //		}
 //	}
 //}
+
+namespace MusiC.Extensions.Windows
+{
+	public class Hamming : Window
+	{
+		public Hamming(int size, int overlap) : base("Hamming", size, overlap)
+		{
+		}
+		
+		override public double Factory(int n)
+		{
+			//return 0.53836 - 0.46164 * (Math.Cos(2 * Math.PI * n / (m_size - 1)));
+			return 0.54 - 0.46 * (Math.Cos(2 * Math.PI * n / (m_size - 1)));
+		}
+	}
+}
