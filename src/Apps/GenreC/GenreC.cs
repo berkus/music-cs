8d64dacab0cefb1733c0d9464694f72a23723a02
mjsmagalhaes <mjsmagalhaes@a630a9a0-c198-11dd-84b3-df5d23adf0f1>
2009-01-27 17:50:45 +0000

    UPDATE: Copyright Notice.

--- src/Apps/GenreC/GenreC.cs
+++ src/Apps/GenreC/GenreC.cs
@@ -1,6 +1,6 @@
 /*
  * The MIT License
- * Copyright (c) 2008 Marcos José Sant'Anna Magalhães
+ * Copyright (c) 2008-2009 Marcos José Sant'Anna Magalhães
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -19,7 +19,6 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
- * 
  */
 
 using System;
@@ -31,7 +30,7 @@ namespace MusiC.Apps
 	/// 
 	/// This application just uses the library to run an specific music genre classification algorithm.
 	/// </summary>
-	/// <see cref="MusiC.Extensions.Classifiers.Barbedo"
+	/// <see cref="MusiC.Extensions.Classifiers.Barbedo"></see>
 	class GenreC
 	{
 		static void Main(String[] args)
--- src/Extensions/Handlers/DBHandler/DBHandler.cpp
+++ src/Extensions/Handlers/DBHandler/DBHandler.cpp
@@ -1,6 +1,6 @@
 /*
  * The MIT License
- * Copyright (c) 2008 Marcos José Sant'Anna Magalhães
+ * Copyright (c) 2008-2009 Marcos José Sant'Anna Magalhães
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -19,9 +19,8 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
- *
  */
- 
+
 #include <fstream>
 #include <iostream>
 #include <sstream>
@@ -36,7 +35,7 @@ string tabulate(int t)
 {
 	ostringstream o;
 
-	for(int i = 0; i < t-1; i++)	
+	for(int i = 0; i < t-1; i++)
 		o << "\t";
 
 	o << t << ")";
@@ -57,7 +56,7 @@ string tabulate(int t)
 	#define LOG_IN() t++;cout << tabulate(t) <<  __FUNCTION__ << endl
 	#define LOG_OUT() cout << tabulate(t) <<  __FUNCTION__ << endl;t--
 #else
-	#define LOG_IN() // 
+	#define LOG_IN() //
 	#define LOG_OUT() //
 #endif
 
@@ -68,25 +67,25 @@ namespace MCFileHandlers
 		class DataHandler
 		{
 			fstream db;
-		
+
 			//Header
 			char * m_sectionLabel;
 			char * m_dataLabel;
 			int m_sectionLabelSz, m_dataLabelSz, m_dataSectionSz;
-		
+
 		public:
-		
+
 			DataHandler()
 			{
 				///@todo: Add Logging initialization
 			}
-		
+
 			~DataHandler()
 			{
 				CloseDB();
 				///@todo: Check/Delete Allocated Data
 			}
-		
+
 			bool IsDBOpen()
 			{
 		LOG_IN();
@@ -94,7 +93,7 @@ namespace MCFileHandlers
 		LOG_OUT();
 				return flag;
 			}
-		
+
 			bool IsEndOfFile()
 			{
 		LOG_IN();
@@ -102,20 +101,20 @@ namespace MCFileHandlers
 		LOG_OUT();
 				return flag;
 			}
-		
+
 			void OpenDB(const char * dbName)
 			{
 		LOG_IN();
 				CloseDB();
-		
+
 				m_dataSectionSz = 0;
 				db.open(dbName, ios_base::in | ios_base::out | ios_base::app | ios_base::binary);
-				
+
 				LOG("Opening:" << dbName);
 				LOG("Status:" << db.is_open());
 		LOG_OUT();
 			}
-		
+
 			void CloseDB()
 			{
 		LOG_IN();
@@ -125,7 +124,7 @@ namespace MCFileHandlers
 				}
 		LOG_OUT();
 			}
-		
+
 			bool LoadNext()
 			{
 		LOG_IN();
@@ -134,24 +133,24 @@ namespace MCFileHandlers
 		LOG_OUT();
 					return false;
 				}
-		
+
 				if(IsEndOfFile())
 				{
 		LOG_OUT();
 					return false;
 				}
-			
+
 				// Last Header Data Section
 				JumpDataSection();
-			
+
 				// Fileformat
 				ReadHeader();
 				PrintHeader();
-		
+
 		LOG_OUT();
 				return true;
 			}
-		
+
 			void PrintHeader()
 			{
 		LOG_IN();
@@ -160,7 +159,7 @@ namespace MCFileHandlers
 				LOG("Data Size:" << m_dataSectionSz);
 		LOG_OUT();
 			}
-		
+
 			void JumpDataSection()
 			{
 		LOG_IN();
@@ -168,48 +167,48 @@ namespace MCFileHandlers
 				db.seekg(m_dataSectionSz, ios_base::cur);
 		LOG_OUT();
 			}
-		
+
 			void ReadHeader()
 			{
 		LOG_IN();
 				db.read((char *)&m_sectionLabelSz, sizeof(int));LOG("Bytes Read:" << db.gcount());
 				db.read((char *)&m_dataLabelSz, sizeof(int));LOG("Bytes Read:" << db.gcount());
 				db.read((char *)&m_dataSectionSz, sizeof(int));LOG("Bytes Read:" << db.gcount());
-		
+
 				if(m_sectionLabel)
 				{
 					delete m_sectionLabel;
 				}
 				m_sectionLabel = new char [m_sectionLabelSz];
-		
+
 				if(m_dataLabel)
 				{
 					delete m_dataLabel;
 				}
 				m_dataLabel = new char [m_dataLabelSz];
-				
+
 				db.read(m_sectionLabel, m_sectionLabelSz + 1); LOG("Bytes Read:" << db.gcount());
 				db.read(m_dataLabel, m_dataLabelSz + 1); LOG("Bytes Read:" << db.gcount());
 		LOG_OUT();
 			}
-		
+
 			///@brief Returns the data stored in the .db file.
 			///@details trusts that hasData has already positioned get-pointer
 			int ReadData(double * dt)
 			{
 		LOG_IN();
 				long offset = db.tellg();
-		
+
 				dt = new double[m_dataSectionSz/sizeof(double)];
-				db.read((char *)dt, m_dataSectionSz); 
+				db.read((char *)dt, m_dataSectionSz);
 				//LOG("Bytes Read:" << db.gcount());
 				int count = db.gcount();
 				db.seekg(offset, ios_base::beg);
-		
+
 		LOG_OUT();
 				return count;
 			}
-		
+
 			void AddData(const char * sectionLabel, const char * dataLabel, double * data, int dataSectionSz)
 			{
 		LOG_IN();
@@ -218,26 +217,26 @@ namespace MCFileHandlers
 				WriteData(data, dataSectionSz);
 		LOG_OUT();
 			}
-		
+
 			void WriteHeader(const char * sectionLabel, const char * dataLabel, int dataSectionSz)
 			{
 		LOG_IN();
 				int sectionLabelSz = strlen(sectionLabel);
 				LOG("Section Label:`" << sectionLabel << "' - Size:" << sectionLabelSz);
 				db.write((char *) &sectionLabelSz, sizeof(int));LOG("Good:" << !db.fail());
-		
+
 				int dataLabelSz = strlen(dataLabel);
 				LOG("Data Label:`" << dataLabel << "' - Size:" << dataLabelSz);
 				db.write((char *) &dataLabelSz, sizeof(int));LOG("Good:" << !db.fail());
-				
+
 				LOG("Data Section Size:" << dataSectionSz);
 				db.write((char *) &dataSectionSz, sizeof(int));LOG("Good:" << !db.fail());
-		
+
 				db.write(const_cast<char *>(sectionLabel), sectionLabelSz + 1);LOG("Good:" << !db.fail());
 				db.write(const_cast<char *>(dataLabel), dataLabelSz + 1);LOG("Good:" << !db.fail());
 		LOG_OUT();
 			}
-		
+
 			void WriteData(double * data, int dataSectionSz)
 			{
 		LOG_IN();
@@ -246,12 +245,12 @@ namespace MCFileHandlers
 				LOG("Good:" << !db.fail());
 		LOG_OUT();
 			}
-			
+
 			bool HasData(char * sectionLabel, char * dtLabel)
 			{
 		LOG_IN();
 				Reset();
-		
+
 				while(LoadNext())
 				{
 					if(!strcmp(sectionLabel, GetSection()))
@@ -263,21 +262,21 @@ namespace MCFileHandlers
 						}
 					}
 				}
-		
+
 		LOG_OUT();
 				return false;
 			}
-		
+
 			char * GetSection()
 			{
 				return m_sectionLabel;
 			}
-		
+
 			char * GetDataLabel()
 			{
 				return m_dataLabel;
 			}
-		
+
 			void Reset()
 			{
 		LOG_IN();
@@ -287,7 +286,7 @@ namespace MCFileHandlers
 				LOG("After Seek:" << db.tellp());
 		LOG_OUT();
 			}
-		
+
 			void Set()
 			{
 		LOG_IN();
