efb0153c184d798aa32d525086fcc8d6b225f0c0
mjsmagalhaes <mjsmagalhaes@a630a9a0-c198-11dd-84b3-df5d23adf0f1>
2009-05-20 15:58:59 +0000

    ADD: Premake unit test project.

---
+++ build/MusiC.Test.Unit.lua
@@ -0,0 +1,56 @@
+--------------------
+-- Project: MusiC --
+--------------------
+
+project("MusiC.Test.Unit")
+location(base_prj_dir)
+
+-----------
+-- Input --
+-----------
+
+language("c#")
+
+files({
+	base_src_dir.."/MusiC/Test/Unit/**.cs"
+})
+
+includedirs({
+})
+
+links({
+	"System",
+	"System.Core",
+	"MusiC",
+	"log4net"
+})
+
+libdirs({
+	base_bin_dir,
+	base_deps_dir.."/DotNet"
+})
+
+---------------------
+-- Code Generation --
+---------------------
+
+defines({"TRACE"})
+
+configuration({"Debug"})
+	defines({"DEBUG"})
+configuration({})
+
+flags({"Unsafe"})
+
+-- linkoptions({})
+
+------------
+-- Output --
+------------
+
+kind("ConsoleApp")
+
+targetname("MusiC.Test.Unit")
+targetdir(base_bin_dir)
+objdir(base_bin_dir.."/obj")
+
